2e09adb6e9ca4b18e8c14e0eecc6964cb82066b2
Mig29x <miguel.barrera@gmail.com>
2023-01-29 23:53:44 -0300

    Podcast con SR

--- Podcast.py
+++ Podcast.py
@@ -1,5 +1,36 @@
 import feedparser
 import urllib.request
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+import speech_recognition as sr
+
+# Carga el archivo MP3
+audio = AudioSegment.from_file("audio.mp3", format="mp3")
+
+# Convierte el archivo a formato WAV
+audio.export("audio.wav", format="wav")
+
+# Crea chunks de audio
+chunks = make_chunks(audio, chunk_length_ms=5000)
+
+# Instancia de reconocimiento de voz
+r = sr.Recognizer()
+
+# Procesa cada chunk
+for i, chunk in enumerate(chunks):
+    # Guarda el chunk a un archivo temporal
+    chunk.export("chunk{}.wav".format(i), format="wav")
+    with sr.AudioFile("chunk{}.wav".format(i)) as source:
+        audio = r.record(source)
+    # Reconoce el texto en el chunk
+    try:
+        text = r.recognize_google(audio)
+        print(text)
+    except sr.UnknownValueError:
+        print("No se pudo reconocer el texto en el chunk {}".format(i))
+    except sr.RequestError as e:
+        print("Error al reconocer el texto en el chunk {}: {}".format(i, e))
+
 
 rss_url = 'https://nihongoconteppeiz.com/feed/podcast/' # Reemplaza por la URL del RSS del podcast
 feed = feedparser.parse(rss_url)
